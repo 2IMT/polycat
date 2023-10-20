faa061b3da0e90e849944c0c4d55c5037c94e6fc
2IMT <daniil_t04@mail.ru>
2023-10-20 06:40:28 +0300

    Add smoother class

---
+++ src/include/smoother.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <chrono>
+#include <cstdint>
+
+namespace pcat
+{
+
+/**
+ * @brief Makes CPU polling results smooth
+ */
+class smoother
+{
+public:
+    /**
+     * @brief Constructs an instance with specified smoothing period
+     * @param period Period of smoothing
+     */
+    smoother(uint64_t period) noexcept;
+
+    /**
+     * @brief Sets current target for smoother
+     * @param target Target
+     */
+    void target(float target) noexcept;
+
+    /**
+     * @brief Retrieves smoothed value based on delta
+     * @param delta Time difference
+     */
+    float value(uint64_t delta) noexcept;
+
+private:
+    uint64_t m_period;
+    float m_target;
+    float m_prev;
+};
+
+}
---
+++ src/smoother.cpp
@@ -0,0 +1,38 @@
+#include "smoother.h"
+
+#include <cmath>
+
+namespace pcat
+{
+
+smoother::smoother(uint64_t period) noexcept :
+    m_period(period),
+    m_target(0.0f),
+    m_prev(0.0f)
+{
+}
+
+void smoother::target(float target) noexcept { m_target = target; }
+
+float smoother::value(uint64_t delta) noexcept
+{
+    if (m_prev == m_target)
+    {
+        return m_target;
+    }
+
+    float diff = m_target - m_prev;
+    int sign = (diff > 0) - (diff < 0);
+    float step = (float)delta / m_period;
+
+    float result = m_prev;
+    result += step * sign;
+    result =
+        (sign > 0) ? std::min(result, m_target) : std::max(result, m_target);
+
+    m_prev = result;
+
+    return result;
+}
+
+}
