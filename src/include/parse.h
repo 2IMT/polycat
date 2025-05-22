a2ea8c63ad18cb2505d04b08f0fd7dd2363e0b6b
2IMT <daniil_t04@mail.ru>
2025-05-22 22:20:55 +0300

    Document parse class

--- src/include/parse.h
+++ src/include/parse.h
@@ -47,19 +47,34 @@ namespace pcat
         };
 
         /**
-         * @brief Represents parsing error
+         * @brief Thrown on parsing errors
          */
         class err : public std::exception
         {
         public:
             /**
+             * @brief Construct an instance without location
+             * @param message Message
              */
             err(const std::string& message) noexcept;
 
+            /**
+             * @brief Construct an instance with specified location
+             * @param l Location
+             * @param message Message
+             */
             err(loc l, const std::string& message) noexcept;
 
+            /**
+             * @brief Indicates whether whether or not the error has location
+             * @return true - if has, false - otherwise
+             */
             bool has_loc() const noexcept;
 
+            /**
+             * @brief Gets the error location
+             * @return Location
+             */
             loc get_loc() const noexcept;
 
             const char* what() const noexcept override;
@@ -70,6 +85,9 @@ namespace pcat
             std::string m_message;
         };
 
+        /**
+         * @brief Thrown when no key found
+         */
         class no_key_err : public std::exception
         {
         public:
@@ -81,6 +99,9 @@ namespace pcat
             std::string m_message;
         };
 
+        /**
+         * @brief Thrown on type errors
+         */
         class type_err : public std::exception
         {
         public:
@@ -97,18 +118,59 @@ namespace pcat
 
         parse() noexcept;
 
+        /**
+         * @brief Loads the config from file
+         * @param path Path to file
+         * @return List of parsing errors occured
+         */
         std::vector<err> load(const std::string& path) noexcept;
 
+        /**
+         * @brief Gets the string value
+         * @param key Key
+         * @return String value
+         * @exception pcat::parse::no_key_err
+         * @exception pcat::parse::type_err
+         */
         std::string get_string(const std::string& key) const;
 
+        /**
+         * @brief Gets the integer value
+         * @param key Key
+         * @return Integer value
+         * @exception pcat::parse::no_key_err
+         * @exception pcat::parse::type_err
+         */
         int64_t get_int(const std::string& key) const;
 
+        /**
+         * @brief Gets the boolean value
+         * @param key Key
+         * @return Boolean value
+         * @exception pcat::parse::no_key_err
+         * @exception pcat::parse::type_err
+         */
         bool get_bool(const std::string& key) const;
 
+        /**
+         * @brief Gets the value
+         * @param key Key
+         * @return Value
+         * @exception pcat::parse::no_key_err
+         */
         data_value get_value(const std::string& key) const;
 
+        /**
+         * @brief Indicated whether the config has specified key
+         * @param key Key
+         * @return true - if has, false - otherwise
+         */
         bool has_key(const std::string& key) const;
 
+        /**
+         * @brief Get all key-value pairs
+         * @return Unordered map of key-value pairs
+         */
         std::unordered_map<std::string, data_value> values() const;
 
     private:
